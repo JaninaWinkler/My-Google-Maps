9817b2b24453c063ec3a5de23260ae42c0fc6f67
JaninaWinkler <nina.winkler@hotmail.com>
2016-04-07 23:43:04 +0000

    pins with info made.

--- map.js
+++ map.js
@@ -1,75 +1,29 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Info windows</title>
-    <style>
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        height: 100%;
-      }
-      div.#bodyContent {
-        background-color: #EEEEEE;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script>
+function initMap() {
+  var homeLatLng = {lat: 49.2802079, lng: -123.1352891};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: homeLatLng
+  });
 
-      // This example displays a marker at the center of Australia.
-      // When the user clicks the marker, an info window opens.
+  function makeMarker(latitude, longitude, title, content) {
+    var marker = new google.maps.Marker({
+      position: {lat: latitude, lng: longitude},
+      map: map,
+      title: title
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: '<h2>' + title + '</h2>' + '<div>' + content + '</div>'
+    });
+    marker.addListener('click', function(){
+      infowindow.open(map, marker);
+    });
+  };
 
-      function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
+  makeMarker(49.3722894, -123.0994869, 'Grouse Mountain', 'Such a cool place to hang out, go for  hike and enjoy beautiful scenery.');
+  makeMarker(49.2499076, -123.0540892, 'My Apartment', 'Very chill place to relax, do yoga and play music.');
+  makeMarker(49.282228, -123.105889, 'Karma Teachers', 'By far the most genuine and accepting yoga studio in the city. It is always a joy to be here.');
+  makeMarker(49.3017049, -123.1417003, 'Stanley Park', 'No words can describe the endless beauty of this place.');
+  makeMarker(49.2691439, -123.0718541, 'Grandview Park', 'Crazy hippie hangout with very interesting people-watching.');
 
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
+}
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjna8vxnHdBvwfAtMTfaUTTqg-Ep-qs5Q  
-&callback=initMap">
-    </script>
-  </body>
-</html>
